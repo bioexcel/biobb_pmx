10abfc58b05fcf319d53b2d58c26714190a8aeac
Pau Andrio <andriopau@gmail.com>
2019-11-28 15:03:33 +0100

    2019.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biobb_pmx",
-    version="1.0.1",
+    version="2.0.0",
     author="Biobb developers",
     author_email="pau.andrio@bsc.es",
     description="Biobb_pmx is the Biobb module collection to perform PMX (http://pmx.mpibpc.mpg.de) executions.",
@@ -18,7 +18,7 @@ setuptools.setup(
         "Bioexcel": "https://bioexcel.eu/"
     },
     packages=setuptools.find_packages(exclude=['docs',]),
-    install_requires=['biobb_common==1.1.6'],
+    install_requires=['biobb_common==2.0.1'],
     python_requires='==3.6.*',
     classifiers=(
         "Development Status :: 3 - Alpha",
