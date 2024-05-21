3cb23972da0373a978646d6b28c9492b9686d083
Pau Andrio <andriopau@gmail.com>
2024-05-21 12:59:51 +0200

    [DOCS] Adding fair software badge and GA

--- biobb_pmx/__init__.py
+++ biobb_pmx/__init__.py
@@ -1,3 +1,5 @@
+from . import pmxbiobb
+
 name = "biobb_pmx"
 __all__ = ["pmxbiobb"]
 __version__ = "4.1.0"
--- biobb_pmx/pmxbiobb/__init__.py
+++ biobb_pmx/pmxbiobb/__init__.py
@@ -1,2 +1,10 @@
+from . import pmxanalyse
+from . import pmxgentop
+from . import pmxmutate
+from . import pmxatom_mapping
+from . import pmxligand_hybrid
+from . import pmxmerge_ff
+from . import pmxcreate_top
+
 name = "pmxbiobb"
 __all__ = ["pmxanalyse", "pmxgentop", "pmxmutate", "pmxatom_mapping", "pmxligand_hybrid", "pmxmerge_ff", "pmxcreate_top"]
