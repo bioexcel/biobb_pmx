b68818d26fe77b8a22b688cc1ccfbd7371aa6af9
Pau Andrio <andriopau@gmail.com>
2024-05-30 10:56:17 +0200

    [FIX] Fix type hints in conf.py

--- biobb_pmx/docs/source/conf.py
+++ biobb_pmx/docs/source/conf.py
@@ -14,6 +14,7 @@
 
 import sys
 from pathlib import Path
+from typing import List, Dict
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -93,7 +94,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns: List[str] = []
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
@@ -227,7 +228,7 @@ htmlhelp_basename = 'biobb_pmx_doc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
+latex_elements: Dict[str, str] = {
     # The paper size ('letterpaper' or 'a4paper').
     # 'papersize': 'letterpaper',
 
