e0a82faa953b0df6b77b2dec734295e5700a00c9
gbayarri <genis.bayarri@irbbarcelona.org>
2026-05-27 16:27:08 +0200

    Adding containers logic

--- biobb_pmx/test/unitests/test_pmx/test_pmxgentop_container.py
+++ biobb_pmx/test/unitests/test_pmx/test_pmxgentop_container.py
@@ -31,4 +31,4 @@ class TestPmxgentopSingularity:
     def test_pmxgentop_singularity(self):
         pmxgentop(properties=self.properties, **self.paths)
         assert fx.not_empty(self.paths['output_top_zip_path'])
-        # assert fx.equal(self.paths['output_top_zip_path'], self.paths['ref_output_top_zip_path'])
+        #assert fx.equal(self.paths['output_top_zip_path'], self.paths['ref_output_top_zip_path'])
